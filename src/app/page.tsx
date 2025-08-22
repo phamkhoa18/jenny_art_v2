15e8ed510f3eb569eb89780e7b992e55dd83b0f8
phamkhoa18 <phamkhoa3092003@gmail.com>
2025-08-22 14:52:40 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,6 @@
-import { Metadata } from "next";
+
 import Header from "./common/Header";
 import Home from "./components/Home";
-
-// SEO cứng cho trang mural
-export const metadata: Metadata = {
-  title: 'Mural - Bộ sưu tập hình ảnh sáng tạo',
-  description:
-    'Khám phá kho hình ảnh chất lượng cao tại Mural. Sáng tạo, hiện đại và đầy cảm hứng.',
-  openGraph: {
-    title: 'Mural - Bộ sưu tập hình ảnh sáng tạo',
-    description:
-      'Khám phá kho hình ảnh chất lượng cao tại Mural. Sáng tạo, hiện đại và đầy cảm hứng.',
-    locale: 'vi_VN',
-    type: 'website',
-  }
-};
 
 
 export default function Page() {
